1094c0db48591ba6f3578ceb4ff7aa8154c5684e
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-08-20 01:30:24 -0300

    trying to make it work on WSL

--- scripts/peon.rb
+++ scripts/peon.rb
@@ -9,8 +9,9 @@ class Peon
       count = instance["count"]
       (1..count).each do |i|
         
-        config.vm.define "#{instance["name"]}-#{i}", primary: instance["type"].eql?("master") do |node|
-          node.vm.hostname = "#{instance["name"]}-#{i}"
+        instance_name = "#{instance["name"]}-#{i}"
+        config.vm.define instance_name, primary: instance["type"].eql?("master") do |node|
+          node.vm.hostname = instance_name
           instance_settings = instance["settings"]
           node.vm.box = instance_settings["box"]
           node.vm.box_check_update = instance_settings["box_check_update"] ||= true
@@ -25,20 +26,28 @@ class Peon
             abort "'pod_network_cidr' is required for instance type 'master'."
           end
           node.vm.provider "virtualbox" do |vb|
+            required_plugins = %w(vagrant-vbguest vagrant-disksize vagrant-share vagrant-sshfs)
+            required_plugins.each do |plugin|
+              unless Vagrant.has_plugin? plugin
+                  system "vagrant plugin install #{plugin}"
+                  _retry=true
+              end
+            end
             node.vbguest.auto_update = instance_settings["vbguest_auto_update"] ||= true
-            node.disksize.size = instance_settings["disk_size"] ||= "20GB"
+            #node.disksize.size = instance_settings["disk_size"] ||= "20GB"
             private_network_ip = network_settings["private_network_ip"].split(".")
-            node_ip = "#{private_network_ip[0]}.#{private_network_ip[1]}.#{private_network_ip[2]}.#{private_network_ip[3].to_i + i}"            
+            node_ip = "#{private_network_ip[0]}.#{private_network_ip[1]}.#{private_network_ip[2]}.#{private_network_ip[3].to_i + i}"
+            #print "Defining instance ##{index} - name: #{instance_name}; type: #{instance["type"]}; ip: #{node_ip}\n"
             if instance["type"].eql?("master") then
               master_node_ip = node_ip
-              masters.push(node_ip)
+              masters.push(instance_name)
             else
-              workers.push(node_ip)
+              workers.push(instance_name)
             end
             
             node.vm.network :private_network, ip: node_ip
 
-            vb.name = "#{instance["name"]}-#{i}"
+            vb.name = instance_name
             vb.linked_clone = true
             vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
             vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
@@ -57,19 +66,22 @@ class Peon
           #  s.name = "Execute Ansible Playbook"
           #  s.inline = "ansible-playbook /vagrant/ansible/playbook.yml -i \"localhost,\" -c local --extra-vars \"node_type=#{instance["type"]} apiserver_advertise_address=#{node_ip} pod_network_cidr=#{pod_network_cidr}\""
           #end
-          print "instance size: #{instances.size}; index: #{index}; i: #{i}\n"
+          #print "instance size: #{instances.size}; index: #{index}; i: #{i}\n"
           if index == instances.size - 1 && i == count then
+            masters = masters.uniq
+            workers = workers.uniq
+            #print "masters: #{masters.size}; workers: #{workers.size}\n"
             node.vm.provision :ansible do |ansible|
               # disable default limit to connect to all the machines
               ansible.limit = "all"
               ansible.playbook = "ansible/playbook.yml"
+              ansible.extra_vars = {
+                "apiserver_advertise_address" => "#{master_node_ip}",
+                "pod_network_cidr" => "#{pod_network_cidr}"
+              }
               ansible.groups = {
                 "masters" => masters,
-                "workers" => workers,
-                "masters:vars" => {
-                  "apiserver_advertise_address" => "#{master_node_ip}",
-                  "pod_network_cidr" => "#{pod_network_cidr}"
-                }
+                "workers" => workers
               }
             end        
           end
